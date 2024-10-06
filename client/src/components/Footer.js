fe5a0107797366a5aa94a3389bb7d438fbde0aa8
MahammadAtheekSkaik <129764770+ShaikMahammadAtheek@users.noreply.github.com>
2024-10-06 16:32:39 +0530

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,9 +4,10 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer-logo">
-                // <div className="logo"></div>
-        // <div className="logo"></div>
-                // <div ><img src={top} alt="" className="logos" /></div>
+                /* <div className="logo"></div>
+         <div className="logo"></div>
+                 <div ><img src={top} alt="" className="logos" /></div>
+                 */
                 <i className="footer-name">JobHustle</i>
             </div>
             <div className="footer-links">
