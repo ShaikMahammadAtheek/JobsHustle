34d63678c215c3c9a69ad0371faffd3eff637740
MahammadAtheekSkaik <129764770+ShaikMahammadAtheek@users.noreply.github.com>
2024-10-08 02:54:01 +0530

    Create Spinner.js

---
+++ client/src/components/Spinner.js
@@ -0,0 +1,15 @@
+// Spinner.js
+import React from 'react';
+import './Spinner.css'; // Import your spinner styles
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      {/* You can customize the spinner here */}
+      <div className="loading">Loading...</div>
+    </div>
+  );
+};
+
+export default Spinner;
+
