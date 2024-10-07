55571d60577f779ffa24af9440266137bcfdf857
MahammadAtheekSkaik <129764770+ShaikMahammadAtheek@users.noreply.github.com>
2024-10-08 03:18:25 +0530

    Update Spinner.js

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -3,9 +3,13 @@ import './Spinner.css'; // Import your spinner styles
 
 const Spinner = () => {
     return (
-        <div className="spinner">
+        {/* <div className="spinner">
             <div className="loading"></div>
         </div>
+        */}
+        <div className="loading-spinner">
+        <div></div>
+      </div>
     );
 };
 
