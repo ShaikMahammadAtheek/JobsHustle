66f0661009b0ece2fb68deb30529820adedb7f23
MahammadAtheekSkaik <129764770+ShaikMahammadAtheek@users.noreply.github.com>
2024-10-18 17:21:33 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -282,7 +282,7 @@ const Navbar = () => {
   // Fetch cities when the dropdown is opened
   useEffect(() => {
     if (isDropdownOpen) {
-      axios.get('https://jobs-hustle.onrender.com/api/cities')
+      axios.get('astik-production.up.railway.app/api/cities')
         .then(response => {
           const cleanCities = [...new Set(response.data.map(city => city.trim()))].sort();
           setDropdownCities(cleanCities);
