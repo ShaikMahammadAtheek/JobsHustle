403734087fb582f444890c6601bb995801c2939b
MahammadAtheekSkaik <129764770+ShaikMahammadAtheek@users.noreply.github.com>
2024-10-22 22:38:38 +0530

    Update Jobss.js

--- client/src/components/Jobss.js
+++ client/src/components/Jobss.js
@@ -6,6 +6,7 @@ function Jobss({job}) {
   return (
     <div className='jobsscol'>
       <div key={job._id} className="job-card">
+            <Link to={`/job/${job._id}`} className="card-link">
             {job.imageUrl && <img src={job.imageUrl} alt={job.title} className="job-card-image" />}
             <h1>{job.title}
             </h1>
@@ -13,7 +14,8 @@ function Jobss({job}) {
             {/*<p>{job.company}</p>*/}
             <p>{job.location}</p>
             <h1>{job.walkInDate && <p className="card-date">{new Date(job.walkInDate).toLocaleDateString()}</p>}</h1>
-            <Link to={`/job/${job._id}`} className="card-link">View Details </Link>
+              </Link> 
+              {/* <Link to={`/job/${job._id}`} className="card-link">View Details </Link> */}
         </div>
     </div>
   )
