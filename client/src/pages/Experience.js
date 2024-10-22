d8b4139b9c4a5e578a8b824084c608f01c48a9bd
MahammadAtheekSkaik <129764770+ShaikMahammadAtheek@users.noreply.github.com>
2024-10-22 23:52:45 +0530

    Update Experience.js

--- client/src/pages/Experience.js
+++ client/src/pages/Experience.js
@@ -30,15 +30,11 @@ const Experience = () => {
       {loading ? ( // Conditional rendering based on loading state
         <Spinner /> // Show spinner while loading
       ) : (
+       
         <div className="carts">
-                    {jobs.map((job) => (
-                        <Card key={job._id} job={job} />
-                    ))}
-                </div>
-        <div className="job-list">
-          {jobs.map(job => (
-            <Jobss key={job._id} job={job} />
-          ))}
+            {jobs.map((job) => (
+                <Card key={job._id} job={job} />
+                ))}
         </div>
       )}
     </div>
